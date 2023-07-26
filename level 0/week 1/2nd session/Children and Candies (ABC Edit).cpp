d95d4d03be3539deb53725f54f5e72f6b97ad286
Servinas Hassan <37937041+servinas@users.noreply.github.com>
2023-07-26 11:34:44 +0300

    Create Children and Candies (ABC Edit).cpp

---
+++ level 0/week 1/2nd session/Children and Candies (ABC Edit).cpp
@@ -0,0 +1,22 @@
+/*link to the problem:
+ * https://atcoder.jp/contests/abc043/tasks/abc043_a?lang=en
+ */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void RUN() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    RUN();
+    int n;
+    cin>>n;
+    int candies=n*(n+1)/2;
+    cout<<candies;
+    return 0;
+}Children and Candies (ABC Edit)
