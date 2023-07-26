3f1a13e142465dc8e1965481ac179201418ed703
Servinas Hassan <37937041+servinas@users.noreply.github.com>
2023-07-26 11:26:37 +0300

    Create Duplex Printing.cpp

---
+++ level 0/week 1/2nd session/Duplex Printing.cpp
@@ -0,0 +1,22 @@
+/*link to the problem:
+ * https://atcoder.jp/contests/abc157/tasks/abc157_a?lang=en
+ */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void RUN() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    RUN();
+    int n;
+    cin>>n;
+    int sheets=n/2+n%2;
+    cout<<sheets;
+    return 0;
+}Duplex Printing
