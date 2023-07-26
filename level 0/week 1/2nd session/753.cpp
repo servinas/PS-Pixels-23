0aa3dd83f371b74a03879792bfd4ee47e116ca30
Servinas Hassan <37937041+servinas@users.noreply.github.com>
2023-07-26 11:20:19 +0300

    Create 753.cpp

---
+++ level 0/week 1/2nd session/753.cpp
@@ -0,0 +1,23 @@
+/*link to the problem:
+ * https://atcoder.jp/contests/abc114/tasks/abc114_a
+ */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void RUN() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+int main() {
+    RUN();
+    int x;
+    cin >> x;
+    if (x == 7 || x == 5 || x == 3)cout << "YES\n";
+    else cout << "NO\n";
+
+    return 0;
+}
